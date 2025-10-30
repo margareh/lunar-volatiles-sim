ddd8772c9d69a9196fd49cbadfe841ba8c379ae0
Maggie Hansen <margareh@andrew.cmu.edu>
2025-10-30 12:35:58 -0400

    Set up survivability analysis

---
+++ scripts/survivability_mc_analysis.py
@@ -0,0 +1,163 @@
+# date:     10-17-2025
+# author:   margaret hansen
+# purpose:  run monte carlo simulations to figure out survivability distribution of 
+#       craters in the face of degradation and impacts
+
+import sys
+import copy
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from multiprocessing import Pool
+from lvsim.sim import LvSim, remove_old_craters
+from lvsim.utils import LvSimCfg
+
+
+# run simulation and compute number of craters destroyed due to diffusion vs impacts
+# at each time step
+def run_sim(lvsim):
+
+    # run sim through all time steps
+    i = 0
+    age_df = pd.DataFrame(columns=['diameter','age','d/D'])
+    while lvsim.t-lvsim.cfg.args.time_delta >= 0:
+
+        # Take a step
+        lvsim.t -= lvsim.cfg.args.time_delta
+        i += 1
+
+        # print progress and store initial set of craters
+        print("Now on " + str(lvsim.t) + " Ga")
+        old_craters_df = copy.copy(lvsim.crater_df)
+
+        # only need to evolve terrain here, don't care about illumination / volatiles
+        # so we can skip those steps
+        lvsim.evolve_terrain()
+
+        # compute number of craters removed
+        new_rows = compute_crater_ages(old_craters_df, lvsim.crater_df)
+
+        # add to database of deleted craters (age, diameter, d/D)
+        age_df = pd.concat([age_df, new_rows])
+
+    return age_df
+
+
+# compute crater ages for craters removed during one time step
+def compute_crater_ages(old_df, new_df):
+    # crater dataframes have columns: x, y, diameter, age, d/D
+
+    # filter to craters that were in old but not new
+    removed_craters = old_df[~old_df.index.isin(new_df.index)]
+    new_craters_np = np.array([new_df.index, new_df.age.values, new_df.diameter.values, new_df.x.values, new_df.y.values]).T
+
+    # determine which new(er) crater removed the older crater
+    # and compute age at which crater was removed
+    with Pool() as p:
+        args = [(i, new_craters_np, None, True) for i in range(removed_craters.shape[0])]
+        out = p.map(remove_old_craters, args) # this produces a list of results
+        # out is a list of tuples: (index of removed crater, age at removal)
+
+    # create dataframe of new rows of removed craters
+    new_ages = [a for (_,a) in out]
+    removed_craters['age'] = new_ages
+    removed_craters.drop(['x', 'y'], inplace=True, axis=1)
+    return removed_craters
+
+
+def get_summary(age_df):
+
+    # add bin column to group by
+    age_df['diam_bin'] = '1-2.5'
+    age_df.loc[[age_df.diameter > 2.5]]['diam_bin'] = '2.5-5'
+    age_df.loc[[age_df.diameter > 5]]['diam_bin'] = '5-10'
+    age_df.loc[[age_df.diameter > 10]]['diam_bin'] = '10-25'
+    age_df.loc[[age_df.diameter > 25]]['diam_bin'] = '25-50'
+    age_df.loc[[age_df.diameter > 50]]['diam_bin'] = '50-75'
+    age_df.loc[[age_df.diameter > 75]]['diam_bin'] = '75-100'
+    age_df.loc[[age_df.diameter > 100]]['diam_bin'] = '100-150'
+    age_df.loc[[age_df.diameter > 150]]['diam_bin'] = '150-200'
+    age_df.loc[[age_df.diameter > 200]]['diam_bin'] = '200-250'
+    age_df.loc[[age_df.diameter > 250]]['diam_bin'] = '250-300'
+    age_df.loc[[age_df.diameter > 300]]['diam_bin'] = '300-350'
+    age_df.loc[[age_df.diameter > 350]]['diam_bin'] = '350-400'
+    age_df.loc[[age_df.diameter > 400]]['diam_bin'] = '400-450'
+    age_df.loc[[age_df.diameter > 450]]['diam_bin'] = '450-500'
+    age_df.loc[[age_df.diameter > 500]]['diam_bin'] = '500-600'
+    age_df.loc[[age_df.diameter > 600]]['diam_bin'] = '600-700'
+    age_df.loc[[age_df.diameter > 700]]['diam_bin'] = '700-800'
+    age_df.loc[[age_df.diameter > 800]]['diam_bin'] = '800-900'
+    age_df.loc[[age_df.diameter > 900]]['diam_bin'] = '900-1000'
+    age_df.set_index('diam_bin')
+    age_df.drop('diameter', axis=1, inplace=True)
+
+    # compute aggregate statistics
+    counts_df = age_df.groupby(by=['diam_bin']).count()
+    counts_df.drop('d/D', axis=1, inplace=True)
+    counts_df.rename(columns={'age': 'count'})
+
+    avgs_df = age_df.groupby(by=['diam_bin']).mean()
+    avgs_df.rename(columns={'age': 'avg_age', 'd/D': 'avg_dD'})
+
+    stdevs_df = age_df.groupby(by=['diam_bin']).std()
+    stdevs_df.rename(columns={'age': 'stdev_age', 'd/D': 'stdev_dD'})
+
+    # combine into one final dataframe
+    summ_df = pd.concat([counts_df, avgs_df, stdevs_df], axis=1)
+    return summ_df
+
+
+# main program
+if __name__ == "__main__":
+
+    # print(sys.argv)
+
+    # addtl_args = {'test3': 'C',
+    #               'test4': 'D'}
+
+    # args_to_parse = copy.copy(sys.argv)
+    # for k, v in addtl_args.items():
+    #     args_to_parse.append('--'+k)
+    #     args_to_parse.append(v)
+    # print(args_to_parse)
+
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dim', type=int, default=200, help='Dimension of one side of region')
+    parser.add_argument('--surface_age', type=float, default=4250, help='Surface age in Myr')
+    parser.add_argument('--time_step', type=float, default=0.01, help='Time step in Myr')
+    parser.add_argument('--iters', type=int, default=100, help='Iterations of MC sim to run')
+    args = parser.parse_args()
+
+    # add to arguments to make sure sim has everything it needs
+    cfg = LvSimCfg() # this defines a lot of things for us!
+    cfg.args.use_prod_fn = True
+    cfg.args.max_age = args.surface_age / 1000
+    cfg.args.time_delta = args.time_step / 1000
+    cfg.args.bbox = [0, args.dim, args.dim, 0]
+
+    # run simulations
+    init_seed = 356789746351
+    age_summ_df = pd.DataFrame(columns=['iter', 'diam_bin', 'count', 'avg_age', 'stdev_age', 'avg_dD', 'stdev_dD'])
+    for i in range(args.iters):
+        
+        # setup for this iteration
+        print("Now on " + str(i+1) + " / " + str(args.iters+1))
+        cfg.args.seed = init_seed + i
+        lvsim = LvSim(cfg)
+
+        # run simulation and get results of survivability
+        age_df = run_sim(lvsim)
+
+        # bin by diameter and compute avg and std dev of ages
+        new_age_rows = get_summary(age_df)
+
+        # append to overall dataset
+        age_summ_df = pd.concat([age_summ_df, new_age_rows])
+        print(len(age_summ_df))
+
+    # plot the results
+
+
