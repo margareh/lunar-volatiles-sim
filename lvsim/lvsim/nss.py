0d3612511a470bd720b506142715f73dd870e8ed
Maggie Hansen <margareh@andrew.cmu.edu>
2025-12-05 16:20:56 -0500

    Add NSS model and test script

---
+++ lvsim/lvsim/nss.py
@@ -0,0 +1,46 @@
+# date:     12-5-2025
+# author:   margaret hansen
+# purpose:  generate nss observations for ice depth and fraction
+
+import pandas as pd
+import numpy as np
+
+from scipy.interpolate import RectBivariateSpline
+
+# NSS sensor model class
+# contains ability to do forward (get wt % and depth given NSS obs) and 
+#   inverse (simulate NSS obs given wt % and depth) modeling
+class NSS():
+
+    def __init__(self, file):
+        # load file with sensor data
+        data_pd = pd.read_csv(file)
+        self.data = data_pd.to_numpy() # wt pct, depth, det1, det2
+
+        # create interpolation grid
+        self.setup()
+
+    # forward model (depth and wt % from observations)
+    def forward(self, obs1, obs2):
+        pred_d = self.depth(obs1, obs2)
+        pred_wt = self.wt_pct(obs1, obs2)
+        return pred_d, pred_wt
+
+    # inverse model (simulated obs from depth and wt %)
+    def inverse(self, wt_pct, depth, noise=False):
+        pred1 = self.det1(depth, wt_pct)
+        pred2 = self.det2(depth, wt_pct)
+        # simulated poisson noise if desired
+        # taken from NeoGeographyToolkit/vipersci
+        if noise:
+            pred1, pred2 = np.random.default_rng().poisson(lam=(pred1, pred2))
+        return pred1, pred2
+
+    # create interpolation grid based on direction of model
+    def setup(self):
+        data_rshp = self.data.reshape((15, 21, 4))
+        # order of dataset: depth, wt %, detector 1 counts, detector 2 counts
+        self.det1 = RectBivariateSpline(np.unique(self.data[...,1]), np.unique(self.data[...,0]), data_rshp[...,2].T)
+        self.det2 = RectBivariateSpline(np.unique(self.data[...,1]), np.unique(self.data[...,0]), data_rshp[...,3].T)
+        self.depth = RectBivariateSpline(np.unique(self.data[...,2]), np.unique(self.data[...,3]), data_rshp[...,1].T)
+        self.wt_pct = RectBivariateSpline(np.unique(self.data[...,2]), np.unique(self.data[...,3]), data_rshp[...,0].T)
---
+++ scripts/test_nss_model.py
@@ -0,0 +1,63 @@
+# date:     12-5-2025
+# author:   margaret hansen
+# purpose:  test nss measurement creation from depth and fraction from moonpies
+#   note: default ice and regolith densities are those used in moonpies and are 
+#   cited as being from Cannon (2020)
+
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from lvsim.nss import NSS
+
+
+if __name__ == "__main__":
+
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--map_path', type=str, help='File path to data with depth and ice fraction to use')
+    parser.add_argument('--nss_file', type=str, default='../data/leaf_LP.csv', help='File path to file with NSS calibration data')
+    parser.add_argument('--map_res', type=float, default=1., help='Map resolution in meters per pixel')
+    parser.add_argument('--ice_density', type=float, default=934., help='Ice density to use (kg / m^3)')
+    parser.add_argument('--reg_density', type=float, default=1500., help='Regolith density to use (kg / m^3)')
+    args = parser.parse_args()
+
+    # load data on ice depth and fraction
+    map_data = np.load(os.path.join(args.map_path, 'data.npz'))
+    ice_depth = map_data['ice_depth']
+    ice_frac = map_data['ice_frac']
+    ice_tot = np.sum(map_data['ice_col_grid'], axis=0)
+    n,m = ice_depth.shape
+
+    # convert fraction to wt %
+    ice_wt = (ice_tot * (args.map_res**2)) * args.ice_density
+    ej_tot = (ice_tot * (1-ice_frac)) / ice_frac
+    ej_wt = (ej_tot * (args.map_res**2)) * args.reg_density    
+    ice_wt_pct = ice_wt / (ice_wt + ej_wt)
+
+    # reshape inputs
+    depth = ice_depth.reshape((n*m))
+    wt_pct = ice_wt_pct.reshape((n*m))
+
+    # generate NSS sensor observations
+    nss = NSS(args.nss_file)
+    obs1, obs2 = nss.inverse(wt_pct, depth)
+    obs1_map = obs1.reshape((n,m))
+    obs2_map = obs2.reshape((n,m))
+
+    # display NSS observations
+    fig, ax = plt.subplots(2,2,figsize=(20,20))
+
+    ax[0,0].imshow(ice_depth, cmap='Blues')
+    ax[0,1].imshow(ice_wt_pct, cmap='Oranges')
+    ax[1,0].imshow(obs1_map, cmap='PuRd_r')
+    ax[1,1].imshow(obs2_map, cmap='BuGr_r')
+
+    ax[0,0].set_title('Depth')
+    ax[0,1].set_title('Wt %')
+    ax[1,0].set_title('Det 1 Count')
+    ax[1,1].set_title('Det 2 Count')
+
+    plt.show()
+    # plt.savefig(os.path.join(args.map_path, 'nss_obs.png'), dpi=100, bbox_inches='tight')
